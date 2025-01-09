54c12c8b21e2892a792193659c1a8877eb26b267
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-01-09 11:28:43 +0530

    updated: cart management

--- src/Components/CartList.jsx
+++ src/Components/CartList.jsx
@@ -1,5 +1,6 @@
-import { FaShoppingCart, FaStar, FaTrash } from "react-icons/fa";
+import { FaStar, FaTrash } from "react-icons/fa";
 import { useCart } from "../Providers/CartProvider";
+import toast from "react-hot-toast";
 
 const Cart = () => {
 
@@ -10,7 +11,23 @@ const Cart = () => {
         setCartList(res)
     }
 
-    return <div className="mt-5">
+    const handleQtyUpdate = (operator, product) => {
+
+        if (operator == "-" && product.quantity == 1) {
+            handleRemove(product.id)
+            return toast.success("Product removed from cart")
+        }
+
+        const updatedCart = cartList.map(item => {
+            if (item.id == product.id) {
+                return {...item, quantity: operator == "+" ? item.quantity + 1 : item.quantity - 1}
+            }
+            return item
+        })
+        setCartList(updatedCart)
+    }
+
+    return <div className="mt-5 bg-secondary">
         <h1 className="text-center">Cart</h1>
         <div className="d-flex gap-2 justify-content-center flex-wrap">
            {cartList.map((product) => {
@@ -35,12 +52,12 @@ const Cart = () => {
                            <div className={`d-flex align-items-center gap-1 ${product.rating < 2 ? "text-danger" : product.rating >= 2 && product.rating < 4 ? "text-warning" : "text-success"}`}>
                              <FaStar /> {product.rating}
                            </div>
-                           <div>${product.price}</div>
+                           <div>${(product.price * product.quantity).toFixed(2)}</div>
                                </div>
                                <div className="my-2 d-flex justify-content-between align-items-center">
-                                   <button className="btn btn-success w-100">-</button>
+                                   <button className="btn btn-success w-100" onClick={() => handleQtyUpdate("-", product)}>-</button>
                                    <div className=" w-100 text-center">{product.quantity}</div>
-                                   <button className="btn btn-success w-100">+</button>
+                                   <button className="btn btn-success w-100" onClick={() => handleQtyUpdate("+", product)}>+</button>
                                </div>
                          <button className="btn btn-danger w-100 mt-2" onClick={() => handleRemove(product.id)}>
                            <FaTrash /> Remove
