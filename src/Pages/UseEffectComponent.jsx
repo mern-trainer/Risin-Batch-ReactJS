447ed455ad140d28d553b678e57820dc0fb12e60
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-01-14 10:28:29 +0530

    use effect - example

--- src/Pages/UseEffectComponent.jsx
+++ src/Pages/UseEffectComponent.jsx
@@ -11,22 +11,43 @@ import { useEffect, useState } from "react"
 
 const UseEffectComponent = () => {
 
+    // const [counter, setCounter] = useState(0)
+    // const [counter2, setCounter2] = useState(0)
+
+    // useEffect(() => {
+    //     console.log(counter);
+    //     return () => {
+    //         console.log("unmounting");
+    //     }
+    // }, [counter])
+
+    // return <div>
+    //     {counter}
+    //     {counter2}
+    //     <button onClick={() => setCounter(counter + 1)}>Click</button>
+    //     <button onClick={() => setCounter2(counter2 + 1)}>Click</button>
+    // </div>
+
     const [counter, setCounter] = useState(0)
-    const [counter2, setCounter2] = useState(0)
+    const [timer, setTimer] = useState(0)
 
     useEffect(() => {
-        console.log(counter);
+        const timeInterval = setInterval(() => {
+            setTimer((prevTime) => prevTime + 1)
+        }, 1000) // --ms
         return () => {
-            console.log("unmounting");
+            clearInterval(timeInterval)
         }
     }, [counter])
 
-    return <div>
-        {counter}
-        {counter2}
-        <button onClick={() => setCounter(counter + 1)}>Click</button>
-        <button onClick={() => setCounter2(counter2 + 1)}>Click</button>
+    return <div className="d-flex flex-column gap-3 align-items-center mt-4">
+        <div>Timer: {timer}</div>
+        <div>counter: {counter}</div>
+        <div>
+            <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>Update Count</button>
+        </div>
     </div>
+
 }
 
 export default UseEffectComponent
